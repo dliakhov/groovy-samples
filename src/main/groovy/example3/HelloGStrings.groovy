a7f3cb6e9aa587410c72ca46df25bbdd75104435
dmytro <dy.lyahov@gmail.com>
2018-09-13 07:46:16 +0300

    Add multiline strings

---
+++ src/main/groovy/example3/HelloGStrings.groovy
@@ -0,0 +1,32 @@
+package example3
+
+class HelloGStrings {
+
+    def name
+
+    HelloGStrings(name) {
+        this.name = name
+    }
+
+    def sayHelloString() {
+        'Hello ' + name
+    }
+
+    def sayHelloGString() {
+        "Hello ${name}"
+    }
+
+    def sayHelloMultilineString() {
+        '''
+            Hello
+            ''' + name
+    }
+
+    def sayHelloMultilineGString() {
+        """
+            Hello
+            ${name}
+        """
+    }
+
+}
---
+++ src/test/groovy/example3/HelloGStringsTest.groovy
@@ -0,0 +1,58 @@
+package example3
+
+import spock.lang.Specification
+
+class HelloGStringsTest extends Specification {
+
+    def helloGStrings
+    def setup() {
+        helloGStrings = new HelloGStrings("Test")
+    }
+
+    def "check sayHelloString execution"() {
+        when:
+        def result = helloGStrings.sayHelloString()
+
+        then:
+        result == 'Hello Test'
+    }
+
+    def "check sayHelloGString execution"() {
+        when:
+        def result = helloGStrings.sayHelloGString()
+
+        then:
+        result == 'Hello Test' // call compareTo then equals!
+        !result.equals('Hello Test') // false here
+    }
+
+    def "check sayHelloMultilineString execution"() {
+        given:
+        def expectedResult =
+        '''
+            Hello
+            Test'''
+
+        when:
+        def result = helloGStrings.sayHelloMultilineString()
+
+        then:
+        result == expectedResult
+    }
+
+    def "check sayHelloMultilineGString execution"() {
+        given:
+        def expectedResult =
+        """
+            Hello
+            Test
+        """
+
+        when:
+        def result = helloGStrings.sayHelloMultilineGString()
+
+        then:
+        result == expectedResult
+    }
+
+}
