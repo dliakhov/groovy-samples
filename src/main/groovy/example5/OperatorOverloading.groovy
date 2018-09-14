7d7c2e4cfb2d73d82611e199c31a26e24cb22d8e
dmytro <dy.lyahov@gmail.com>
2018-09-14 07:09:15 +0300

    Add example overloading operator

---
+++ src/main/groovy/example5/OperatorOverloading.groovy
@@ -0,0 +1,18 @@
+package example5
+
+class Fly {
+    double weight
+
+    Fly plus(Cutlet cutlet) {
+        new Fly(weight: this.weight + cutlet.weight)
+    }
+}
+
+class Cutlet {
+    double weight
+
+    Fly plus(Fly fly) {
+        new Fly(weight: this.weight + fly.weight / 2)
+    }
+
+}
---
+++ src/test/groovy/example5/OperatorOverloadingTest.groovy
@@ -0,0 +1,33 @@
+package example5
+
+import spock.lang.Specification
+
+class OperatorOverloadingTest extends Specification {
+
+    def "add fly and cutlet"() {
+        given:
+        def fly = new Fly(weight: 2)
+        def cutlet = new Cutlet(weight: 5)
+
+        when:
+        def result = fly + cutlet
+
+        then:
+        result instanceof Fly
+        Double.compare(result.weight, 7) == 0
+    }
+
+    def "add cutlet and fly"() {
+        given:
+        def fly = new Fly(weight: 2)
+        def cutlet = new Cutlet(weight: 5)
+
+        when:
+        def result = cutlet + fly
+
+        then:
+        result instanceof Fly
+        Double.compare(result.weight, 6) == 0
+    }
+
+}
