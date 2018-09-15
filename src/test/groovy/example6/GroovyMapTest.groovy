5acfd218c7a96cbe8cdb9399219fb8ebb903c469
dmytro <dy.lyahov@gmail.com>
2018-09-15 08:15:15 +0300

    Add test for collections

---
+++ src/test/groovy/example6/GroovyMapTest.groovy
@@ -0,0 +1,46 @@
+package example6
+
+import spock.lang.Specification
+
+class GroovyMapTest extends Specification {
+
+    def "show map in Groovy"() {
+        setup:
+        def map
+        def emptyMap
+
+        when:
+        map = [1:"1", 2:true, 3:1.2]
+        emptyMap = [:]
+
+        then:
+        map.size() == 3
+        map instanceof HashMap
+        map[2] == true
+
+        emptyMap.size() == 0
+    }
+
+    def "show map in Groovy 2"() {
+        setup:
+        def map
+
+        when:
+        map = [a: 1, b: 2, c: 3]
+        map.c = 5 // access to element
+
+        map['d'] = 10 // add new one
+        map.e = 11 // add another one
+
+        then:
+        map.a == 1
+        map['b'] == 2
+        map.c == 5
+
+        map.d == 10
+        map.e == 11
+    }
+
+
+
+}
---
+++ src/test/groovy/example6/GroovyRangeAndListTest.groovy
@@ -0,0 +1,76 @@
+package example6
+
+import spock.lang.Specification
+
+class GroovyRangeAndListTest extends Specification {
+
+    def "check range operator"() {
+        given:
+        def rangeStep2
+        def rangeStep3
+
+        when:
+        rangeStep2 = (1..10).step(2)
+        rangeStep3 = (2.5..10).step(3)
+
+        then:
+        0..5 == [0, 1, 2, 3, 4, 5]
+        0..<5 == [0, 1, 2, 3, 4] // exclusive
+        5..0 == [5, 4, 3, 2, 1, 0] // range in decreasing value
+        rangeStep2 == [1, 3, 5, 7, 9]
+        rangeStep3 == [2.5, 5.5, 8.5]
+    }
+
+    def "show list in Groovy"() {
+        given:
+        def list
+        def elem
+        def bigList
+
+        when:
+        list = [0, 1, 2, 3]
+        elem = list[2]
+        bigList = (1..100).toList()
+
+        then:
+        list.size() == 4
+        list instanceof ArrayList
+        elem == 2
+        bigList instanceof ArrayList
+    }
+
+    def "show list in Groovy 2"() {
+        given:
+        def bigList
+
+        when:
+        bigList = (1..100).toList()
+
+        then:
+        bigList[0..3] == [1, 2, 3, 4]
+        bigList[0, 5, 99] == [1, 6, 100]
+
+        bigList[-1] == 100
+        bigList[-1..-4] == [100, 99, 98, 97]
+    }
+
+
+    def "show methods in list"() {
+        given:
+        def list
+
+        when:
+        list = (1..5).toList()
+        def findTwo = list.find {it % 2 == 0}
+        def findEven = list.findAll {it % 2 == 0}
+
+        def store = 0
+        list.each { store += it}
+
+        then:
+        findTwo == 2
+        findEven == [2, 4]
+        store == 15
+    }
+
+}
