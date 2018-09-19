d95838962067ca7f78b5f338e1fbb5091c2ee50e
Dmytro Liakhov <DLiakhov@agileassets.com>
2018-09-19 07:56:27 +0300

    Update closure example

--- src/main/java/example10/ExampleDsl.groovy
+++ src/main/java/example10/ExampleDsl.groovy
@@ -4,12 +4,10 @@ def show = {println it}
 def square_root = { Math.sqrt(it) }
 
 def please(action) {
-    return [the: { what ->
+    [the: { what ->
         [of: {action(what(it))}]
     }]
 }
 
-
-please(show).the(square_root).of(100)
 
 please show the square_root of 100
